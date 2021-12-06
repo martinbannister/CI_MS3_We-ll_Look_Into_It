34ca99b8405705d29714264139741d0b9e6e8fd1
martinbannister <martin.bannister91@gmail.com>
2021-12-06 13:33:44 +0000

    FEAT(app.py): add the flask app

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ import os
 from flask import Flask
 if os.path.exists("env.py"):
     import env.py
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_reports():
+    return "Getting reports..."
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
