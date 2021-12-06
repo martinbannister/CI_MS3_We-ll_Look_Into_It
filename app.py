e72ad8e632434eaa016b54ea97011eab4d908f2f
martinbannister <martin.bannister91@gmail.com>
2021-12-06 19:18:57 +0000

    FEAT(app.py): add more imports and app config

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import os
-from flask import Flask
+from flask import (Flask, flash, render_template, redirect,
+                    request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env.py
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
+@app.route("/get_reports")
 def get_reports():
     return "Getting reports..."
 
