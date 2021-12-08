be671a9b62e67e15b34087befe742c5edb84ee82
martinbannister <martin.bannister91@gmail.com>
2021-12-08 21:03:18 +0000

    FEAT(flash_messages.js): add toast styles

--- static/js/flash_messages.js
+++ static/js/flash_messages.js
@@ -1,4 +1,15 @@
 function flash_toast(msg, category) {
     // use category for styling
-    M.toast({html: msg });
+    let styling = 'rounded';
+    switch(category) {
+        case 'flash_error':
+            styling += ' orange darken-4';
+            break;
+        case 'flash_success':
+            styling += ' lime accent-4 grey-text text-darken-4';
+            break;
+        default:
+
+    }
+    M.toast({html: msg, classes: styling });
   }
