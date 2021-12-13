9f36fd9265eb40f71b447d2b0a0c26bb0d432d3c
martinbannister <martin.bannister91@gmail.com>
2021-12-13 21:47:33 +0000

    FEAT(base.html): apply FAB toolbar & custom style

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
     let FABs = document.querySelectorAll('.fixed-action-btn');
-    M.FloatingActionButton.init(FABs);
+    M.FloatingActionButton.init(FABs, {toolbarEnabled: true});
     let selectElems = document.querySelectorAll('select');
     if (selectElems) {
       M.FormSelect.init(selectElems);
