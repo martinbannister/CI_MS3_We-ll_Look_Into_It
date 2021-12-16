4b3d7fcf3c4ad3d4f222ebb591d4e58927319200
martinbannister <martin.bannister91@gmail.com>
2021-12-16 09:23:38 +0000

    FEAT(app.py): add 404 & 500 errorhandlers

--- app.py
+++ app.py
@@ -21,6 +21,18 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# ---------------------------- CUSTOM 404 PAGE -------------------------
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# ---------------------------- CUSTOM 500 PAGE -------------------------
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+
 # ---------------------------- DEFAULT / GET POTHOLES -------------------------
 @app.route("/")
 @app.route("/get_potholes")
